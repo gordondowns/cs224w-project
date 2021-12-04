c68ca01d4dfeb6404b61451d2fb26cdc4eb37cd4
Gordon Downs <gordiedowns@gmail.com>
2021-12-04 15:51:27 -0800

    load asymmetric units of a directory of CIFs (on Windows)

--- src/data/load_crystal_structures.py
+++ src/data/load_crystal_structures.py
@@ -29,7 +29,7 @@ def direct_coords_to_cartesian_coords(atom_xyz_direct,cell_params):
     return atom_xyz_cartesian
 
 
-def load_single_crystal_structure(file_path, verbose=False):
+def load_single_crystal_structure(file_path, maximum_distance_for_edge = np.inf, verbose=False):
     
     mineral_name = file_path.split('\\')[-1].split('/')[-1].split('0')[0]
     out = read_cif(file_path)
@@ -63,11 +63,24 @@ def load_single_crystal_structure(file_path, verbose=False):
     for i1,xyz1 in enumerate(atom_xyz_cartesian[:-1]):
         for i2,xyz2 in enumerate(atom_xyz_cartesian[i1+1:],start=i1+1):
             dist = np.linalg.norm(xyz1-xyz2)
-            # if dist < 3.0:
-            G.add_edge(i1,i2,dist=dist)
+            if dist < maximum_distance_for_edge:
+                G.add_edge(i1,i2,dist=dist)
 
     if verbose:
         drawing_labels = nx.get_node_attributes(G,'element')
         nx.draw(G,labels=drawing_labels)
 
     return mineral_name, G
+
+
+def load_cif_data(
+        CIF_directory_path='../data/raw/CIFs/',
+        verbose=False):
+    file_paths_list = glob(CIF_directory_path+'*.cif')
+    mineral_names = []
+    graphs = []
+    for fp in file_paths_list:
+        mineral_name, graph = load_single_crystal_structure(fp, verbose=verbose)
+        mineral_names.append(mineral_name)
+        graphs.append(graph)
+    return file_paths_list, mineral_names, graphs
