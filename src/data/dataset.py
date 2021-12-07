69fa2dd4a7f5371560b95678196d27c646d29a58
Gordon Downs <gordiedowns@gmail.com>
2021-12-07 13:52:50 -0800

    first attempt where training starts to run

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,9 +2,9 @@ from typing import Optional, Callable, List
 
 import torch
 from sklearn.model_selection import train_test_split
-from torch_geometric.data import (InMemoryDatset)
+from torch_geometric.data import InMemoryDataset
 
-class Crystals(InMemoryDatset):
+class Crystals(InMemoryDataset):
     def __init__(
         self, 
         root: str, 
@@ -13,10 +13,14 @@ class Crystals(InMemoryDatset):
         pre_filter: Optional[Callable] = None
     ):
         super().__init__(root, transform, pre_transform, pre_filter)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+        self.data, self.slices = torch.load(root)
     
     def get_splits(self, deterministic=True):
         seed = 0 if deterministic else None
-        train, val_test = train_test_split(self.data, test_size=.3, random_state=seed)
+        train, val_test = train_test_split(self, test_size=.3, random_state=seed)
         val, test = train_test_split(val_test, test_size=.5, random_state=seed)
         return train, val, test
+
+    @property
+    def processed_file_names(self) -> str:
+        return ['data/processed/data_v1.pt']
